82cf39e0bb2c2a839dea7ef12d5fb08c41e4f495
Bhaskar <bhaskar@cowhite.com>
2018-06-19 10:58:49 +0530

    Disquc comments.

--- django_cowhite_cms/views.py
+++ django_cowhite_cms/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic as generic_views
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from django.conf import settings
 
 from .models import *
 
@@ -12,5 +13,7 @@ class PageView(generic_views.TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(PageView, self).get_context_data(*args, **kwargs)
         context['page'] = get_object_or_404(Page, slug=kwargs['slug'])
+        if hasattr(settings, 'DISQUS_SHORT_NAME'):
+            context['disqus_short_name'] = settings.DISQUS_SHORT_NAME
         return context
 
